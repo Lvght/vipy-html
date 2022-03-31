c49624bd95644930891038559fb958278a5b5201
lucas-cid <lucas_mcid@hotmail.com>
2022-03-31 15:10:41 -0300

    logout buttom

--- templates/js/_navbar.js
+++ templates/js/_navbar.js
@@ -4,6 +4,12 @@ $(document).ready(function () {
   client.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
       $("#navbar").html(client.responseText);
+
+      $("#logoutIcon").click(function (event) {
+        document.cookie =
+          "user" + "=;path=/;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+        window.location.href = "/";
+      });
     }
   };
   client.send();
