4fdb918a197662e9a05b32707d92ad0df5a8813f
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2022-04-01 00:12:48 -0300

    Profille edit working
    Co-Authored-By: AndreLRoJr <89857946+AndreLRoJr@users.noreply.github.com>
    Co-Authored-By: Lucas-Cid <52435102+Lucas-Cid@users.noreply.github.com>
    Co-authored-by: OakBranches.M <matheusramosimperador@gmail.com>

--- js/profile.js
+++ js/profile.js
@@ -1,4 +1,4 @@
-import { Request } from './utils.js'
+import { getUser, Request } from './utils.js'
 
 $(document).ready(function () {
     const request = new Request({
@@ -14,6 +14,26 @@ $(document).ready(function () {
         },
         onError: (error) => {
             console.log(error)
+        }
+    })
+
+    request.send()
+})
+
+// Envia o formulário de edição de perfil.
+$(document).on('click', '#submitEditProfileButton', function (e) {
+
+    const request = new Request({
+        type: "PATCH",
+        hasAuth: true,
+        url: "/profiles/" + window.location.href.split('?id=')[1] + "/",
+        data: JSON.stringify(Object.fromEntries(new FormData(document.getElementById('editProfileForm')).entries())),
+        onSuccess: (responseData) => {
+            console.log(responseData);
+            window.location.href = "./profile.html?id=" + getUser().id;
+        },
+        onError: (error) => {
+            console.error(error)
         }
     })
 
