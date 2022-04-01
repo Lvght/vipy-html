8e2eee360b3b54869fc893edf1ffba782a18d079
lucas-cid <lucas_mcid@hotmail.com>
2022-04-01 12:40:22 -0300

    Fix profile logic
    Co-Authored-By: AndreLRoJr <89857946+AndreLRoJr@users.noreply.github.com>
    Co-authored-by: OakBranches <matheusramosimperador@gmail.com>
    Co-authored-by: vinql <viniciusquaresma1904@gmail.com>

--- js/profile.js
+++ js/profile.js
@@ -1,105 +1,137 @@
-import { User, getUser, setUser, Request } from './utils.js'
+import { User, getUser, setUser, Request } from "./utils.js";
 
 function getPersonalityText(personality) {
-    switch (personality) {
-        case 1: return "ISTJ"
-        case 2: return "ISFJ"
-        case 3: return "INFJ"
-        case 4: return "INTJ"
-        case 5: return "ISTP"
-        case 6: return "ISFP"
-        case 7: return "INFP"
-        case 8: return "INTP"
-        case 9: return "ESTP"
-        case 10: return "ESFP"
-        case 11: return "ENFP"
-        case 12: return "ENTP"
-        case 13: return "ESTJ"
-        case 14: return "ESFJ"
-        case 15: return "ENFJ"
-        case 16: return "ENTJ"
-        default: return "Não definido"
-    }
+  switch (personality) {
+    case 1:
+      return "ISTJ";
+    case 2:
+      return "ISFJ";
+    case 3:
+      return "INFJ";
+    case 4:
+      return "INTJ";
+    case 5:
+      return "ISTP";
+    case 6:
+      return "ISFP";
+    case 7:
+      return "INFP";
+    case 8:
+      return "INTP";
+    case 9:
+      return "ESTP";
+    case 10:
+      return "ESFP";
+    case 11:
+      return "ENFP";
+    case 12:
+      return "ENTP";
+    case 13:
+      return "ESTJ";
+    case 14:
+      return "ESFJ";
+    case 15:
+      return "ENFJ";
+    case 16:
+      return "ENTJ";
+    default:
+      return "Não definido";
+  }
 }
 
 $(document).ready(function () {
-    const request = new Request({
-        type: "GET",
-        hasAuth: true,
-        url: "/profiles/" + window.location.href.split('?id=')[1] + "/",
+  const profileId = window.location.href.split("?id=")[1];
+  const currentUserId = getUser().id;
 
-        onSuccess: (responseData) => {
-            console.log(responseData);
+  if (profileId != currentUserId) {
+    $("#editProfileButton").hide();
+    $("#excludeProfileButton").hide();
+  }
 
-            const user = new User(responseData)
-            const oldUser = getUser()
-            
-            user.tokens = oldUser.tokens
+  const request = new Request({
+    type: "GET",
+    hasAuth: true,
+    url: "/profiles/" + window.location.href.split("?id=")[1] + "/",
 
-            setUser(user)
+    onSuccess: (responseData) => {
+      console.log(responseData);
 
-            document.getElementById('displayName').innerText = user.display_name;
-            document.getElementById('username').innerText = '@' + user.username;
-            document.getElementById('biography').innerText = user.biography;
-            document.getElementById('almaMater').innerText = user.alma_mater;
-            document.getElementById('city').innerText = user.city;
-            document.getElementById('contactNumber').innerText = user.contact_number;
-            document.getElementById('personality').innerText = getPersonalityText(user.personality)
+      const user = new User(responseData);
+      const oldUser = getUser();
 
-            // Injeta os valores iniciais no formulário de edição de perfil.
-            document.getElementById('edit-displayName').value = user.display_name;
-            document.getElementById('edit-birthday').value = user.birthday
-            document.getElementById('edit-almaMater').value = user.alma_mater;
-            document.getElementById('edit-biography').value = user.biography;
-            document.getElementById('edit-city').value = user.city;
+      user.tokens = oldUser.tokens;
 
-            const dropdown = document.getElementById('edit-personality')
-            dropdown.value = user.personality
-            
-            document.getElementById('edit-contactNumber').value = user.contact_number;
-        },
-        onError: (error) => {
-            console.log(error)
-        }
-    })
+      document.getElementById("displayName").innerText = user.display_name;
+      document.getElementById("username").innerText = "@" + user.username;
+      document.getElementById("biography").innerText = user.biography;
+      document.getElementById("almaMater").innerText = user.alma_mater;
+      document.getElementById("city").innerText = user.city;
+      document.getElementById("contactNumber").innerText = user.contact_number;
+      document.getElementById("personality").innerText = getPersonalityText(
+        user.personality
+      );
 
-    request.send()
-})
+      // Injeta os valores iniciais no formulário de edição de perfil.
+      document.getElementById("edit-displayName").value = user.display_name;
+      document.getElementById("edit-birthday").value = user.birthday;
+      document.getElementById("edit-almaMater").value = user.alma_mater;
+      document.getElementById("edit-biography").value = user.biography;
+      document.getElementById("edit-city").value = user.city;
+
+      const dropdown = document.getElementById("edit-personality");
+      dropdown.value = user.personality;
+
+      document.getElementById("edit-contactNumber").value = user.contact_number;
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  request.send();
+});
 
 // Envia o formulário de edição de perfil.
-$(document).on('click', '#submitEditProfileButton', function (e) {
+$(document).on("click", "#submitEditProfileButton", function (e) {
+  const request = new Request({
+    type: "PATCH",
+    hasAuth: true,
+    url: "/profiles/" + window.location.href.split("?id=")[1] + "/",
+    data: JSON.stringify(
+      Object.fromEntries(
+        new FormData(document.getElementById("editProfileForm")).entries()
+      )
+    ),
+    onSuccess: (responseData) => {
+      const newUser = new User(responseData);
+      const oldUser = getUser();
+      newUser.tokens = oldUser.tokens;
 
-    const request = new Request({
-        type: "PATCH",
-        hasAuth: true,
-        url: "/profiles/" + window.location.href.split('?id=')[1] + "/",
-        data: JSON.stringify(Object.fromEntries(new FormData(document.getElementById('editProfileForm')).entries())),
-        onSuccess: (responseData) => {
-            console.log(responseData);
-            window.location.href = "./profile.html?id=" + getUser().id;
-        },
-        onError: (error) => {
-            console.error(error)
-        }
-    })
+      setUser(newUser);
+      window.location.href = "./profile.html?id=" + getUser().id;
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
 
-    request.send()
-})
+  request.send();
+});
 
-$(document).on('click', '#excludeProfileButton', function (e) {
-    const request = new Request({
-        type: "DELETE",
-        hasAuth: true,
-        url: "/profiles/" + window.location.href.split('?id=')[1] + "/",
-        onSuccess: (responseData) => {
-            window.location = '/'
-        },
-        onError: (e) => {
-            console.error(e)
-        }
-    })
+$(document).on("click", "#excludeProfileButton", function (e) {
+  const request = new Request({
+    type: "DELETE",
+    hasAuth: true,
+    url: "/profiles/" + window.location.href.split("?id=")[1] + "/",
+    onSuccess: (responseData) => {
+      window.location = "/";
+    },
+    onError: (e) => {
+      console.error(e);
+    },
+  });
 
-    if (confirm('Você tem certeza de que deseja excluir sua conta?')) {
-        request.send()
-    }
-})
+  if (confirm("Você tem certeza de que deseja excluir sua conta?")) {
+    request.send();
+  }
+});
